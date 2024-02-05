17838938a13a2b7b8b6f54ab22bc0a37dabb37ba
Ben Frame <brewerytattooer@gmail.com>
2024-02-05 15:51:08 -0500

    wip

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ class Seasons {
     if(this.currentSeason === 3){
       this.currentSeason = 0
     } else {
-      this.currentSeason++
+      ++this.currentSeason
     }
     return result
   }
@@ -120,6 +120,8 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -137,6 +139,15 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if(distance <= milesCanDrive){
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+      return this.odometer
+    }
+    this.odometer = this.odometer + milesCanDrive
+    this.tank = 0 
+    return this.odometer
   }
 
   /**
@@ -152,6 +163,13 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if(gallons <= gallonsThatFit){
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
